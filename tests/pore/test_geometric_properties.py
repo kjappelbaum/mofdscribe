d1947e0947d1f80c6fda2b85e62173854db216a3
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-04-22 18:19:24 +0200

    chore: uses tolerance in pytest for zeo++

--- tests/pore/test_geometric_properties.py
+++ tests/pore/test_geometric_properties.py
@@ -9,6 +9,7 @@ from mofdscribe.pore.geometric_properties import (
     _parse_sa_zeopp,
     _parse_volpo_zeopp,
 )
+from pytest import approx
 
 SA_SAMPLE_OUTPUT = """@ EDI.sa Unitcell_volume: 307.484   Density: 1.62239
 
@@ -28,11 +29,11 @@ RES_SAMPLE_OUTPUT = "output_file.res    1.70107 0.95106  1.64805"
 
 def test_parse_sa():
     parsed = _parse_sa_zeopp(SA_SAMPLE_OUTPUT)
-    assert parsed["unitcell_volume"] == 307.484
-    assert parsed["density"] == 1.62239
-    assert parsed["asa_a2"] == 60.7713
-    assert parsed["asa_m2cm3"] == 1976.4
-    assert parsed["asa_m2g"] == 1218.21
+    assert parsed["unitcell_volume"] == approx(307.484, 0.1)
+    assert parsed["density"] == approx(1.62239, 0.1)
+    assert parsed["asa_a2"] == approx(60.7713, 0.1)
+    assert parsed["asa_m2cm3"] == approx(1976.4, 0.1)
+    assert parsed["asa_m2g"] == approx(1218.21, 0.1)
     assert parsed["nasa_a2"] == 0
     assert parsed["nasa_m2cm3"] == 0
     assert parsed["nasa_m2g"] == 0
@@ -49,11 +50,11 @@ def test_parse_res():
 
 def test_parse_volpo_zeopp():
     res = _parse_volpo_zeopp(VOLPO_SAMPLE_OUTPUT)
-    assert res["unitcell_volume"] == 307.484
-    assert res["density"] == 1.62239
-    assert res["av_a3"] == 22.6493
-    assert res["av_volume_fraction"] == 0.07366
-    assert res["av_cm3g"] == 0.0454022
+    assert res["unitcell_volume"] == approx(307.484, 0.1)
+    assert res["density"] == approx(1.62239, 0.1)
+    assert res["av_a3"] == approx(22.6493, 0.1)
+    assert res["av_volume_fraction"] == approx(0.07366, 0.1)
+    assert res["av_cm3g"] == approx(0.0454022, 0.1)
     assert res["nav_a3"] == 0
     assert res["nav_volume_fraction"] == 0
     assert res["nav_cm3g"] == 0
@@ -64,9 +65,9 @@ def test_pore_diameters(hkust_structure):
     assert pd.feature_labels() == ["lis", "lifs", "lifsp"]
     result = pd.featurize(hkust_structure)
     assert len(result) == 3
-    assert result[0] == 13.21425
-    assert result[1] == 6.66829
-    assert result[2] == 13.21425
+    assert result[0] == approx(13.21425, 0.1)
+    assert result[1] == approx(6.66829, 0.1)
+    assert result[2] == approx(13.21425, 0.1)
 
 
 def test_surface_area(hkust_structure):
@@ -84,10 +85,11 @@ def test_surface_area(hkust_structure):
     result = sa.featurize(hkust_structure)
     assert len(result) == 8
 
-    assert result[0] == 1.82808e04
-    assert result[1] == 8.79097e-01
-    assert result[3] == 2.80901e03
-    assert result[4] == 3.19533e03
+    assert result[0] == approx(1.82808e04, 0.1)
+    assert result[1] == approx(8.79097e-01, 0.1)
+    assert result[2] == approx(5.13510e03, 0.1)
+    assert result[3] == approx(2.80901e03, 0.1)
+    assert result[4] == approx(3.19533e03, 0.1)
     assert result[5] == 0
     assert result[6] == 0
     assert result[7] == 0
@@ -108,10 +110,11 @@ def test_accessible_volume(hkust_structure):
 
     result = av.featurize(hkust_structure)
     assert len(result) == 8
-    assert result[0] == 1.82808e04
-    assert result[1] == 8.79097e-01
-    assert result[3] == 7.40000e-01
-    assert result[4] == 8.41773e-01
+    assert result[0] == approx(1.82808e04, 0.1)
+    assert result[1] == approx(8.79097e-01, 0.1)
+    assert result[2] == approx(1.35278e04, 0.1)
+    assert result[3] == approx(7.40000e-01, 0.1)
+    assert result[4] == approx(8.41773e-01, 0.1)
     assert result[5] == 0
     assert result[6] == 0
     assert result[7] == 0
@@ -123,7 +126,7 @@ def test_raytracing_histogram(hkust_structure):
     assert len(rth.citations()) == 2
     features = rth.featurize(hkust_structure)
     assert len(features) == 1000
-    assert features[0] > 0
+    assert features[0] >= 1.0
 
 
 def test_psd(hkust_structure):
