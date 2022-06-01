9b0cb883840aa1e6b1916bcfed0fac8cbdacf1fe
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-06-01 17:16:23 +0200

    docs: update docstring

--- src/mofdscribe/chemistry/amd.py
+++ src/mofdscribe/chemistry/amd.py
@@ -7,11 +7,14 @@ from pymatgen.core import IStructure, Structure
 
 from mofdscribe.utils.substructures import filter_element
 
-__all__ = ['AMD']
+__all__ = ["AMD"]
 
 
 class AMD(BaseFeaturizer):
     """
+    Implements the average minimum distance (AMD) isometry invariant.
+    The AMD is the average of the point-wise distance distribution (PDD) of a crystal. The PDD lists distances to neighbouring atoms in order, closest first. Hence, the kth AMD value corresponds to the average distance to the kth nearest neighbour.
+
     The descriptors can be computed over the full structure or substructures of certain atom types.
     """
 
@@ -19,9 +22,9 @@ class AMD(BaseFeaturizer):
         self,
         k: int = 100,
         atom_types=(
-            'C-H-N-O',
-            'F-Cl-Br-I',
-            'Cu-Mn-Ni-Mo-Fe-Pt-Zn-Ca-Er-Au-Cd-Co-Gd-Na-Sm-Eu-Tb-V-Ag-Nd-U-Ba-Ce-K-Ga-Cr-Al-Li-Sc-Ru-In-Mg-Zr-Dy-W-Yb-Y-Ho-Re-Be-Rb-La-Sn-Cs-Pb-Pr-Bi-Tm-Sr-Ti-Hf-Ir-Nb-Pd-Hg-Th-Np-Lu-Rh-Pu',
+            "C-H-N-O",
+            "F-Cl-Br-I",
+            "Cu-Mn-Ni-Mo-Fe-Pt-Zn-Ca-Er-Au-Cd-Co-Gd-Na-Sm-Eu-Tb-V-Ag-Nd-U-Ba-Ce-K-Ga-Cr-Al-Li-Sc-Ru-In-Mg-Zr-Dy-W-Yb-Y-Ho-Re-Be-Rb-La-Sn-Cs-Pb-Pr-Bi-Tm-Sr-Ti-Hf-Ir-Nb-Pd-Hg-Th-Np-Lu-Rh-Pu",
         ),
         compute_for_all_elements: bool = True,
     ) -> None:
@@ -36,7 +39,7 @@ class AMD(BaseFeaturizer):
         atom_types = [] if atom_types is None else atom_types
         self.elements = atom_types
         self.atom_types = (
-            list(atom_types) + ['all'] if compute_for_all_elements else list(atom_types)
+            list(atom_types) + ["all"] if compute_for_all_elements else list(atom_types)
         )
         self.compute_for_all_elements = compute_for_all_elements
 
@@ -44,7 +47,7 @@ class AMD(BaseFeaturizer):
         labels = []
         for atom_type in self.atom_types:
             for i in range(self.k):
-                labels.append(f'{atom_type}_{i}')
+                labels.append(f"{atom_type}_{i}")
 
         return labels
 
@@ -90,17 +93,17 @@ class AMD(BaseFeaturizer):
 
     def citations(self):
         return [
-            '@article{amd2022,'
-            'title = {Average Minimum Distances of periodic point sets - foundational invariants for mapping periodic crystals},'
-            'author = {Daniel Widdowson and Marco M Mosca and Angeles Pulido and Vitaliy Kurlin and Andrew I Cooper},'
-            'journal = {MATCH Communications in Mathematical and in Computer Chemistry},'
-            'doi = {10.46793/match.87-3.529W},'
-            'volume = {87},'
-            'number = {3},'
-            'pages = {529-559},'
-            'year = {2022}'
-            '}'
+            "@article{amd2022,"
+            "title = {Average Minimum Distances of periodic point sets - foundational invariants for mapping periodic crystals},"
+            "author = {Daniel Widdowson and Marco M Mosca and Angeles Pulido and Vitaliy Kurlin and Andrew I Cooper},"
+            "journal = {MATCH Communications in Mathematical and in Computer Chemistry},"
+            "doi = {10.46793/match.87-3.529W},"
+            "volume = {87},"
+            "number = {3},"
+            "pages = {529-559},"
+            "year = {2022}"
+            "}"
         ]
 
     def implementors(self):
-        return ['Kevin Maik Jablonka']
+        return ["Kevin Maik Jablonka"]
