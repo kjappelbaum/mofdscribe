a7186163b7bd38206354d4899f495f236b390d60
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-06-14 23:39:05 +0200

    feat: implement fingerprint splitter

--- src/mofdscribe/splitters/splitters.py
+++ src/mofdscribe/splitters/splitters.py
@@ -3,6 +3,8 @@ from collections import Counter
 
 import numpy as np
 
+__all__ = ("DensitySpliiter", "HashSplitter", "TimeSplitter", "Splitter")
+
 
 class Splitter:
     def train_valid_test_split(
@@ -14,6 +16,9 @@ class Splitter:
         shuffle: bool = True,
         **kwargs,
     ):
+        for frac in [frac_train, frac_valid]:
+            if frac >= 1.0:
+                raise ValueError("frac_train and frac_valid must be < 1.0")
         if sample_frac > 1.0:
             raise ValueError("sample_frac must be <= 1.0")
         number_of_points = int(len(ds) * sample_frac)
@@ -28,32 +33,39 @@ class Splitter:
         ]
         test_inds = indices[number_of_train_points + number_of_valid_points :]
 
+        if shuffle:
+            np.random.shuffle(train_inds)
+            np.random.shuffle(valid_inds)
+            np.random.shuffle(test_inds)
         return train_inds, valid_inds, test_inds
 
-    def train_valid_test_split(
+    def train_test_split(
         self,
         ds,
         frac_train: float,
-        frac_valid: float,
         sample_frac: float = 1.0,
         shuffle: bool = True,
         **kwargs,
     ):
+        if frac_train >= 1.0:
+            raise ValueError("frac_train and frac_test must be < 1.0")
+
         if sample_frac > 1.0:
             raise ValueError("sample_frac must be <= 1.0")
         number_of_points = int(len(ds) * sample_frac)
         number_of_train_points = int(frac_train * number_of_points)
-        number_of_valid_points = int(frac_valid * number_of_points)
 
         indices = self.get_sorted_indices(ds, shuffle=shuffle)
 
         train_inds = indices[:number_of_train_points]
-        valid_inds = indices[
-            number_of_train_points : number_of_train_points + number_of_valid_points
-        ]
-        test_inds = indices[number_of_train_points + number_of_valid_points :]
 
-        return train_inds, valid_inds, test_inds
+        test_inds = indices[number_of_train_points:]
+
+        if shuffle:
+            np.random.shuffle(train_inds)
+            np.random.shuffle(test_inds)
+
+        return train_inds, test_inds
 
     def k_fold(self, ds, k, shuffle: bool = True):
         """
@@ -72,6 +84,10 @@ class Splitter:
             test_mask[test_index] = True
             train_fold = indices[np.logical_not(test_mask)]
             test_fold = indices[test_mask]
+            if shuffle:
+                np.random.shuffle(train_fold)
+                np.random.shuffle(test_fold)
+
             yield train_fold, test_fold
             current = stop
 
@@ -155,4 +171,26 @@ class TimeSplitter(Splitter):
         t.sort(reverse=not self.ascending, key=lambda x: (x[0], x[2]))
         indices = [i for _, i, _ in t]
 
+        return indices
+
+
+class RandomSplitter(Splitter):
+    def get_sorted_indices(self, ds, shuffle: bool = True):
+        indices = np.arange(len(ds))
+        if shuffle:
+            np.random.shuffle(indices)
+
+        return indices
+
+
+class FingerprintSplitter(Splitter):
+    def __init__(
+        self,
+        feature_names,
+    ) -> None:
+        self.feature_names = feature_names
+        super().__init__()
+
+    def get_sorted_indices(self, ds, shuffle: bool = True):
+        indices = ds._df.sort_values(by=self.feature_names).index.values
         return indices
--- tests/splitters/test_splitters.py
+++ tests/splitters/test_splitters.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from mofdscribe.datasets.core_dataset import CoREDataset
-from mofdscribe.splitters.splitters import HashSplitter, TimeSplitter, DensitySpliiter
+from mofdscribe.splitters.splitters import (
+    DensitySpliiter,
+    HashSplitter,
+    TimeSplitter,
+    RandomSplitter,
+    FingerprintSplitter,
+)
 
 
 def test_hash_splitter():
@@ -12,6 +18,10 @@ def test_hash_splitter():
         assert set(train) & set(test) == set()
         assert len(set(list(train) + list(test))) == len(ds)
 
+    splits = hs.train_valid_test_split(ds, frac_train=0.5, frac_valid=0.3, shuffle=True)
+    assert len(splits) == 3
+    assert len(splits[0]) > len(splits[1]) > len(splits[2])
+
 
 def test_time_splitter():
     ds = CoREDataset()
@@ -22,6 +32,10 @@ def test_time_splitter():
         assert set(train) & set(test) == set()
         assert len(set(list(train) + list(test))) == len(ds)
 
+    splits = ts.train_valid_test_split(ds, frac_train=0.5, frac_valid=0.3, shuffle=True)
+    assert len(splits) == 3
+    assert len(splits[0]) > len(splits[1]) > len(splits[2])
+
 
 def test_density_splitter():
     ds = CoREDataset()
@@ -31,3 +45,215 @@ def test_density_splitter():
 
         assert set(train) & set(test) == set()
         assert len(set(list(train) + list(test))) == len(ds)
+
+    splits = dens_splitter.train_valid_test_split(ds, frac_train=0.5, frac_valid=0.3, shuffle=True)
+    assert len(splits) == 3
+    assert len(splits[0]) > len(splits[1]) > len(splits[2])
+
+
+def test_random_splitter():
+    ds = CoREDataset()
+
+    rs = RandomSplitter()
+    for train, test in rs.k_fold(ds, k=5, shuffle=True):
+
+        assert set(train) & set(test) == set()
+        assert len(set(list(train) + list(test))) == len(ds)
+
+    splits = rs.train_valid_test_split(ds, frac_train=0.5, frac_valid=0.3, shuffle=True)
+    assert len(splits) == 3
+    assert len(splits[0]) > len(splits[1]) > len(splits[2])
+
+
+def test_fingerprint_splitter():
+    ds = CoREDataset()
+
+    fps = FingerprintSplitter(
+        feature_names=[
+            "total_POV_gravimetric",
+            "mc_CRY-chi-0-all",
+            "mc_CRY-chi-1-all",
+            "mc_CRY-chi-2-all",
+            "mc_CRY-chi-3-all",
+            "mc_CRY-Z-0-all",
+            "mc_CRY-Z-1-all",
+            "mc_CRY-Z-2-all",
+            "mc_CRY-Z-3-all",
+            "mc_CRY-I-0-all",
+            "mc_CRY-I-1-all",
+            "mc_CRY-I-2-all",
+            "mc_CRY-I-3-all",
+            "mc_CRY-T-0-all",
+            "mc_CRY-T-1-all",
+            "mc_CRY-T-2-all",
+            "mc_CRY-T-3-all",
+            "mc_CRY-S-0-all",
+            "mc_CRY-S-1-all",
+            "mc_CRY-S-2-all",
+            "mc_CRY-S-3-all",
+            "D_mc_CRY-chi-0-all",
+            "D_mc_CRY-chi-1-all",
+            "D_mc_CRY-chi-2-all",
+            "D_mc_CRY-chi-3-all",
+            "D_mc_CRY-Z-0-all",
+            "D_mc_CRY-Z-1-all",
+            "D_mc_CRY-Z-2-all",
+            "D_mc_CRY-Z-3-all",
+            "D_mc_CRY-I-0-all",
+            "D_mc_CRY-I-1-all",
+            "D_mc_CRY-I-2-all",
+            "D_mc_CRY-I-3-all",
+            "D_mc_CRY-T-0-all",
+            "D_mc_CRY-T-1-all",
+            "D_mc_CRY-T-2-all",
+            "D_mc_CRY-T-3-all",
+            "D_mc_CRY-S-0-all",
+            "D_mc_CRY-S-1-all",
+            "D_mc_CRY-S-2-all",
+            "D_mc_CRY-S-3-all",
+            "sum-mc_CRY-chi-0-all",
+            "sum-mc_CRY-chi-1-all",
+            "sum-mc_CRY-chi-2-all",
+            "sum-mc_CRY-chi-3-all",
+            "sum-mc_CRY-Z-0-all",
+            "sum-mc_CRY-Z-1-all",
+            "sum-mc_CRY-Z-2-all",
+            "sum-mc_CRY-Z-3-all",
+            "sum-mc_CRY-I-0-all",
+            "sum-mc_CRY-I-1-all",
+            "sum-mc_CRY-I-2-all",
+            "sum-mc_CRY-I-3-all",
+            "sum-mc_CRY-T-0-all",
+            "sum-mc_CRY-T-1-all",
+            "sum-mc_CRY-T-2-all",
+            "sum-mc_CRY-T-3-all",
+            "sum-mc_CRY-S-0-all",
+            "sum-mc_CRY-S-1-all",
+            "sum-mc_CRY-S-2-all",
+            "sum-mc_CRY-S-3-all",
+            "sum-D_mc_CRY-chi-0-all",
+            "sum-D_mc_CRY-chi-1-all",
+            "sum-D_mc_CRY-chi-2-all",
+            "sum-D_mc_CRY-chi-3-all",
+            "sum-D_mc_CRY-Z-0-all",
+            "sum-D_mc_CRY-Z-1-all",
+            "sum-D_mc_CRY-Z-2-all",
+            "sum-D_mc_CRY-Z-3-all",
+            "sum-D_mc_CRY-I-0-all",
+            "sum-D_mc_CRY-I-1-all",
+            "sum-D_mc_CRY-I-2-all",
+            "sum-D_mc_CRY-I-3-all",
+            "sum-D_mc_CRY-T-0-all",
+            "sum-D_mc_CRY-T-1-all",
+            "sum-D_mc_CRY-T-2-all",
+            "sum-D_mc_CRY-T-3-all",
+            "sum-D_mc_CRY-S-0-all",
+            "sum-D_mc_CRY-S-1-all",
+            "sum-D_mc_CRY-S-2-all",
+            "sum-D_mc_CRY-S-3-all",
+            "D_lc-chi-0-all",
+            "D_lc-chi-1-all",
+            "D_lc-chi-2-all",
+            "D_lc-chi-3-all",
+            "D_lc-Z-0-all",
+            "D_lc-Z-1-all",
+            "D_lc-Z-2-all",
+            "D_lc-Z-3-all",
+            "D_lc-I-0-all",
+            "D_lc-I-1-all",
+            "D_lc-I-2-all",
+            "D_lc-I-3-all",
+            "D_lc-T-0-all",
+            "D_lc-T-1-all",
+            "D_lc-T-2-all",
+            "D_lc-T-3-all",
+            "D_lc-S-0-all",
+            "D_lc-S-1-all",
+            "D_lc-S-2-all",
+            "D_lc-S-3-all",
+            "D_lc-alpha-0-all",
+            "D_lc-alpha-1-all",
+            "D_lc-alpha-2-all",
+            "D_lc-alpha-3-all",
+            "D_func-chi-0-all",
+            "D_func-chi-1-all",
+            "D_func-chi-2-all",
+            "D_func-chi-3-all",
+            "D_func-Z-0-all",
+            "D_func-Z-1-all",
+            "D_func-Z-2-all",
+            "D_func-Z-3-all",
+            "D_func-I-0-all",
+            "D_func-I-1-all",
+            "D_func-I-2-all",
+            "D_func-I-3-all",
+            "D_func-T-0-all",
+            "D_func-T-1-all",
+            "D_func-T-2-all",
+            "D_func-T-3-all",
+            "D_func-S-0-all",
+            "D_func-S-1-all",
+            "D_func-S-2-all",
+            "D_func-S-3-all",
+            "D_func-alpha-0-all",
+            "D_func-alpha-1-all",
+            "D_func-alpha-2-all",
+            "D_func-alpha-3-all",
+            "sum-D_lc-chi-0-all",
+            "sum-D_lc-chi-1-all",
+            "sum-D_lc-chi-2-all",
+            "sum-D_lc-chi-3-all",
+            "sum-D_lc-Z-0-all",
+            "sum-D_lc-Z-1-all",
+            "sum-D_lc-Z-2-all",
+            "sum-D_lc-Z-3-all",
+            "sum-D_lc-I-0-all",
+            "sum-D_lc-I-1-all",
+            "sum-D_lc-I-2-all",
+            "sum-D_lc-I-3-all",
+            "sum-D_lc-T-0-all",
+            "sum-D_lc-T-1-all",
+            "sum-D_lc-T-2-all",
+            "sum-D_lc-T-3-all",
+            "sum-D_lc-S-0-all",
+            "sum-D_lc-S-1-all",
+            "sum-D_lc-S-2-all",
+            "sum-D_lc-S-3-all",
+            "sum-D_lc-alpha-0-all",
+            "sum-D_lc-alpha-1-all",
+            "sum-D_lc-alpha-2-all",
+            "sum-D_lc-alpha-3-all",
+            "sum-D_func-chi-0-all",
+            "sum-D_func-chi-1-all",
+            "sum-D_func-chi-2-all",
+            "sum-D_func-chi-3-all",
+            "sum-D_func-Z-0-all",
+            "sum-D_func-Z-1-all",
+            "sum-D_func-Z-2-all",
+            "sum-D_func-Z-3-all",
+            "sum-D_func-I-0-all",
+            "sum-D_func-I-1-all",
+            "sum-D_func-I-2-all",
+            "sum-D_func-I-3-all",
+            "sum-D_func-T-0-all",
+            "sum-D_func-T-1-all",
+            "sum-D_func-T-2-all",
+            "sum-D_func-T-3-all",
+            "sum-D_func-S-0-all",
+            "sum-D_func-S-1-all",
+            "sum-D_func-S-2-all",
+            "sum-D_func-S-3-all",
+            "sum-D_func-alpha-0-all",
+            "sum-D_func-alpha-1-all",
+            "sum-D_func-alpha-2-all",
+            "sum-D_func-alpha-3-all",
+        ]
+    )
+    for train, test in fps.k_fold(ds, k=5, shuffle=True):
+
+        assert set(train) & set(test) == set()
+        assert len(set(list(train) + list(test))) == len(ds)
+
+    splits = fps.train_valid_test_split(ds, frac_train=0.5, frac_valid=0.3, shuffle=True)
+    assert len(splits) == 3
+    assert len(splits[0]) > len(splits[1]) > len(splits[2])
