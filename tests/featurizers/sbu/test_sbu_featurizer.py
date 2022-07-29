570152a16c1dfa39f8570276198a935d857e054f
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-07-29 14:05:06 +0200

    feat: add test for SBU featurizer with SOAP

--- tests/featurizers/sbu/test_sbu_featurizer.py
+++ tests/featurizers/sbu/test_sbu_featurizer.py
@@ -3,6 +3,9 @@
 
 from mofdscribe.featurizers.sbu.sbu_featurizer import MOFBBs, SBUFeaturizer
 from mofdscribe.featurizers.topology import PHStats
+from matminer.featurizers.site import SOAP
+from matminer.featurizers.structure import SiteStatsFingerprint
+from pymatgen.core import Structure
 
 
 def test_sbu_featurizer(hkust_structure, molecule):
@@ -20,3 +23,18 @@ def test_sbu_featurizer(hkust_structure, molecule):
     assert features.shape == (768,)
     assert features[0] > 0
     assert features[0] < 2
+
+
+def test_sbu_featurizer_with_matminer_featurizer(hkust_structure):
+    """Test that we can call SBU featurizers with matminer molecules."""
+    base_feat = SiteStatsFingerprint(SOAP.from_preset("formation_energy"))
+    hkust_structure = Structure.from_sites(hkust_structure.sites)
+    base_feat.fit([hkust_structure])
+    featurizer = SBUFeaturizer(base_feat, aggregations=("mean",))
+    features = featurizer.featurize(structure=hkust_structure)
+    assert features.shape == (9504 * 2,)
+    assert features[0] > 0
+    assert features[0] < 2
+
+    linker_feats = [f for f in featurizer.feature_labels() if "linker" in f]
+    assert len(linker_feats) == 9504
