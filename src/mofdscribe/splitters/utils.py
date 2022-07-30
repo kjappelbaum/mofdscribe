a6f0a73bf8385b5651298cef4d00a0ab8b07d648
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-07-30 16:12:20 +0200

    feat: prepare new train/test split

--- src/mofdscribe/splitters/utils.py
+++ src/mofdscribe/splitters/utils.py
@@ -9,7 +9,9 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
-
+from pandas.api.types import infer_dtype
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # todo: can we do here something with numba?
 # for numba, we would need to do some rewrite as there is no support
@@ -115,3 +117,82 @@ def pca_kmeans(
         X_pca = X  # noqa: N806
     kmeans = KMeans(n_clusters=n_clusters, random_state=random_state, **(kmeans_kwargs or {}))
     return kmeans.fit_predict(X_pca)
+
+
+def is_categorical(x):
+    return infer_dtype(x) == "category" or infer_dtype(x) == "integer"
+
+
+def handle_stratification_col(stratification_col):
+    if is_categorical(stratification_col):
+        return stratification_col
+    else:
+        return stratification_col
+
+
+def grouped_stratified_train_test_partition(
+    stratification_col,
+    group_col,
+    train_size,
+    valid_size,
+    test_size,
+    center=np.median,
+    q=[0, 0.25, 0.5, 0.75, 1],
+    shuffle=True,
+    random_state=None,
+):
+    """Grouped stratified train-test partition.
+
+    Note that this won't work well if the number of groups is small.
+    """
+    groups = np.unique(group_col)
+    categorical = is_categorical(stratification_col)
+
+    category_for_group = []
+    # for each group, get the mode
+    if categorical:
+        for group in groups:
+            category_for_group.append(np.mode(stratification_col[group_col == group]))
+    else:
+        for group in groups:
+            category_for_group.append(center(stratification_col[group_col == group]))
+
+    # if we do not have categories, we now need to discretize the stratification_col
+    if not categorical:
+        category_for_group = pd.qcut(category_for_group, q, labels=np.arange(len(q) - 1)).astype(
+            int
+        )
+
+    train_size = int(np.floor(train_size * len(category_for_group)))
+    valid_size = int(np.floor(valid_size * len(category_for_group)))
+    test_size = int(len(category_for_group) - train_size - valid_size)
+
+    # now we can do the split
+    train_groups, test_groups, train_cat, test_cat = train_test_split(
+        groups,
+        category_for_group,
+        train_size=train_size + valid_size,
+        test_size=test_size,
+        stratify=category_for_group,
+        shuffle=shuffle,
+        random_state=random_state,
+    )
+
+    if valid_size > 0:
+        train_groups, valid_groups = train_test_split(
+            train_groups,
+            train_size=train_size,
+            stratify=train_cat,
+            shuffle=shuffle,
+            random_state=random_state,
+        )
+
+    # now, get the original indices
+    train_indices = np.where(np.isin(group_col, train_groups))[0]
+    if valid_size > 0:
+        valid_indices = np.where(np.isin(group_col, valid_groups))[0]
+    else:
+        valid_indices = None
+    test_indices = np.where(np.isin(group_col, test_groups))[0]
+
+    return train_indices, valid_indices, test_indices
--- tests/splitters/test_utils.py
+++ tests/splitters/test_utils.py
@@ -1,8 +1,11 @@
 """Test the splitter helping functions."""
 
 import numpy as np
-
-from mofdscribe.splitters.utils import kennard_stone_sampling
+import pytest
+from mofdscribe.splitters.utils import (
+    kennard_stone_sampling,
+    grouped_stratified_train_test_partition,
+)
 
 
 def test_kennard_stone_sampling():
@@ -18,3 +21,48 @@ def test_kennard_stone_sampling():
 
     indices = kennard_stone_sampling(X, centrality_measure="random")
     assert len(indices) == 3  # we cannot guarantee the order of the indices
+
+
+@pytest.mark.parametrize("number_of_groups", [30, 50, 80, 500, 8_000])
+def test_grouped_stratified_train_test_partition(number_of_groups):
+    # perhaps use hypothesis for fuzzing the data
+
+    datasize = 10_000
+
+    y = np.random.normal(0, 1, size=datasize)
+    groups = np.random.choice(np.arange(number_of_groups), size=datasize)
+
+    train_indices, valid_indices, test_indices = grouped_stratified_train_test_partition(
+        y, groups, train_size=0.5, valid_size=0.25, test_size=0.25
+    )
+
+    assert len(train_indices) + len(test_indices) + len(valid_indices) == datasize
+
+    test_groups = groups[test_indices]
+    train_groups = groups[train_indices]
+    valid_groups = groups[valid_indices]
+    set(test_groups).intersection(set(train_groups)) == set()
+    set(test_groups).intersection(set(valid_groups)) == set()
+    set(train_groups).intersection(set(valid_groups)) == set()
+
+    train_mean = np.median(y[train_indices])
+    valid_mean = np.median(y[valid_indices])
+    test_mean = np.median(y[test_indices])
+    print(train_mean, valid_mean, test_mean)
+    assert (
+        pytest.approx(train_mean, abs=0.1)
+        == pytest.approx(valid_mean, abs=0.1)
+        == pytest.approx(test_mean, abs=0.1)
+    )
+
+    # now, no validation set
+    train_indices, _, test_indices = grouped_stratified_train_test_partition(
+        y, groups, train_size=0.8, valid_size=0.0, test_size=0.2, shuffle=True
+    )
+
+    assert len(train_indices) + len(test_indices) == datasize
+
+    test_groups = groups[test_indices]
+    train_groups = groups[train_indices]
+
+    set(test_groups).intersection(set(train_groups)) == set()
