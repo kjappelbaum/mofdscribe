8cc7a617085ff7c26bccd4b4bd794ebf683a9067
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-08-04 08:47:15 +0200

    wip: start work on tutorial

---
+++ dev_scripts/get_dataset_feats_parallel.py
@@ -0,0 +1,61 @@
+import concurrent.futures
+import os
+from glob import glob
+from pathlib import Path
+from pymatgen.core import Structure
+import click
+import pandas as pd
+from matminer.featurizers.base import MultipleFeaturizer
+
+from mofdscribe.featurizers.chemistry.amd import AMD
+from mofdscribe.featurizers.chemistry.aprdf import APRDF
+from mofdscribe.featurizers.chemistry.racs import RACS
+from mofdscribe.featurizers.pore import PoreDiameters, SurfaceArea
+from mofdscribe.featurizers.topology.ph_stats import PHStats
+from mofdscribe.featurizers.topology.ph_hist import PHHist
+
+featurizer = MultipleFeaturizer(
+    [RACS(), PHStats(), PHHist(), PoreDiameters(), APRDF(), SurfaceArea("CO2"), AMD()]
+)
+
+
+def featurize_mof_ignore_failure(infile, outdir):
+    try:
+        structure = Structure.from_file(infile)
+        feats = featurizer.featurize(structure)
+        features = featurizer.feature_labels()
+        df = pd.DataFrame([dict(zip(features, feats))])
+        inname = Path(infile).stem
+        df.to_csv(os.path.join(outdir, inname + ".csv"))
+
+    except Exception as e:
+        print(f"Failed to featurize {infile} because of {e}")
+
+
+def featurize_multiple_mofs(indir, outdir, start, end, n_jobs=1):
+    all_mof_folders = sorted(os.listdir(indir))
+    already_featurized = glob(os.path.join(outdir, "features_*.csv"))
+    already_featurized = [Path(f).name.split("_")[-1].split(".")[0] for f in already_featurized]
+    mof_folders = all_mof_folders[start:end]
+    mof_folders = [f for f in mof_folders if f not in already_featurized]
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=n_jobs) as executor:
+        for moffolder in mof_folders:
+            executor.submit(featurize_mof_ignore_failure, os.path.join(indir, moffolder), outdir)
+
+
+@click.command("cli")
+@click.option("--indir", "-m", default=None, help="Input directory")
+@click.option("--outdir", type=click.Path(exists=False), default=".")
+@click.option("--start", type=int, default=0)
+@click.option("--end", type=int, default=-1)
+@click.option("--n_jobs", type=int, default=1)
+def cli(indir, outdir, start, end, n_jobs):
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    featurize_multiple_mofs(indir, outdir, start, end, n_jobs)
+
+
+if __name__ == "__main__":
+    cli()
