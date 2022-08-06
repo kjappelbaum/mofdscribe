8587b9aa3f044c20b3d95eb890f098b466401d95
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2022-08-06 19:54:44 +0200

    make it clickable!

--- src/mofdscribe/datasets/qmof_dataset.py
+++ src/mofdscribe/datasets/qmof_dataset.py
@@ -156,17 +156,17 @@ class QMOFDataset(StructureDataset):
         The class will load almost 1GB of data into memory.
 
     References:
-        .. [Rosen2021] Rosen, A. S.; Iyer, S. M.; Ray, D.; Yao, Z.; Aspuru-Guzik, A.; Gagliardi, L.;
+        .. [Rosen2021] `Rosen, A. S.; Iyer, S. M.; Ray, D.; Yao, Z.; Aspuru-Guzik, A.; Gagliardi, L.;
             Notestein, J. M.; Snurr, R. Q. Machine Learning the Quantum-Chemical Properties
             of Metal–Organic Frameworks for Accelerated Materials Discovery.
-            Matter 2021, 4 (5), 1578–1597. https://doi.org/10.1016/j.matt.2021.02.015.
+            Matter 2021, 4 (5), 1578–1597. <https://doi.org/10.1016/j.matt.2021.02.015>`_
 
-        .. [Rosen2022] Rosen, A. S.; Fung, V.; Huck, P.; O'Donnell, C. T.; Horton, M. K.; Truhlar, D. G.;
+        .. [Rosen2022] `Rosen, A. S.; Fung, V.; Huck, P.; O'Donnell, C. T.; Horton, M. K.; Truhlar, D. G.;
             Persson, K. A.; Notestein, J. M.; Snurr, R. Q.
             High-Throughput Predictions of Metal–Organic Framework Electronic Properties:
             Theoretical Challenges, Graph Neural Networks, and Data Exploration.
             npj Computational Materials, 8, 112.
-            https://doi.org/10.1038/s41524-022-00796-6
+            <https://doi.org/10.1038/s41524-022-00796-6>`_
 
     """
 
