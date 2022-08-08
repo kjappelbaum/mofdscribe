87c3488e2a8fd55acf4b1b121f250915306b21fc
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-08-08 19:46:49 +0200

    feat: update bench

--- src/mofdscribe/bench/df_model.py
+++ src/mofdscribe/bench/df_model.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """Helper to build bench model for models that operate on pre-computed feature frames."""
 
-from typing import Iterable
+from typing import Iterable, Optional
+from loguru import logger
 
 
 class DFModel:
-    def __init__(self, model, features: Iterable[str]):
+    def __init__(self, model, features: Optional[Iterable[str]] = None):
         """Initialize the model.
 
         .. note:::
@@ -17,15 +18,25 @@ class DFModel:
         Args:
             model (object): Must implement `fit` and `predict` methods.
                 Using a sklearn function signature will work.
-            feature_df (pd.DataFrame): Feature dataframe.
+            features (Iterable[str], optional): Feature names to use.
+                If not provided, all features will be used.
+                Defaults to None.
         """
         self._model = model
         self._features = features
 
+    @property
+    def features(self):
+        if self._features is None:
+            self._features = list(self.ds.available_features)
+        return self._features
+
     def fit(self, idx, structures, y):
-        X = self.ds._df[self._features].loc[idx, :]  # noqa: N806
+        logger.debug("Fitting model")
+        X = self.ds._df[self.features].loc[idx, :]  # noqa: N806
+        logger.debug(X.shape)
         self._model.fit(X, y)  # noqa: N806
 
     def predict(self, idx, structures):
-        X = self.ds._df[self._features].loc[idx, :]  # noqa: N806
+        X = self.ds._df[self.features].loc[idx, :]  # noqa: N806
         return self._model.predict(X)  # noqa: N806
