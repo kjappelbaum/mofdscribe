2ae63c7d47782211f81bc80847b51c2553ba52bd
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-08-08 18:51:17 +0200

    feat: method to write rst
    closes #328

--- src/mofdscribe/bench/mofbench.py
+++ src/mofdscribe/bench/mofbench.py
@@ -21,6 +21,45 @@ from mofdscribe.version import get_version
 
 __all__ = ["MOFBenchRegression", "BenchResult"]
 
+_RST_TEMPLATE = """{modelname}
+------------------------------------
+
+
+Model card
+..............
+
+Feature set description
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+What features are used?
+#######################
+
+Why are the features informative?
+###################################
+
+
+Why do the features not leak information?
+##############################################
+
+
+Will the features be accessible in real-world test applications?
+###################################################################
+
+Data split
+~~~~~~~~~~
+
+Describe preprocessing steps and how data leakage is avoided
+##############################################################
+
+Describe the feature selection steps and how data leakage is avoided
+#####################################################################
+
+
+Describe the model selection steps and how data leakage is avoided
+#####################################################################
+
+"""
+
 
 class BenchTaskEnum(Enum):
     """Enum for benchmark tasks."""
@@ -67,6 +106,19 @@ class BenchResult(BaseModel):
             "w",
         ) as handle:
             handle.write(self.json())
+
+    def save_rst(self, folder: Union[str, os.PathLike]) -> None:
+        """Prepare RST file for model card and free-text model description."""
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        with open(
+            os.path.join(
+                folder,
+                f"{id_for_bench_result(self)}.rst",
+            ),
+            "w",
+        ) as handle:
+            handle.write(_RST_TEMPLATE.format(modelname=self.name))
 
 
 def id_for_bench_result(bench_result: BenchResult) -> str:
