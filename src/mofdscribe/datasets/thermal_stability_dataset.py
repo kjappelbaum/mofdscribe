481099a1c6bbe1e396c1077e126acc243528b260
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-12-12 13:23:08 +0100

    interface thermal stability dataset Fixes #408

--- src/mofdscribe/datasets/thermal_stability_dataset.py
+++ src/mofdscribe/datasets/thermal_stability_dataset.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""CoRE Dataset."""
+"""Thermal Stability Dataset."""
 import os
 from typing import Collection, Optional, Tuple
 
@@ -12,15 +12,16 @@ from mofdscribe.datasets.checks import check_all_file_exists, length_check
 from mofdscribe.datasets.dataset import AbstractStructureDataset
 from mofdscribe.datasets.utils import compress_dataset
 
-__all__ = ["CoREDataset"]
+__all__ = ["ThermalStabilityDataset"]
 
 
-class CoREDataset(AbstractStructureDataset):
-    """Dataset of gas uptake related features for a subset of CoRE MOFs.
+class ThermalStabilityDataset(AbstractStructureDataset):
+    """Thermal stability for a subset of CoRE MOFs.
 
-    The labels were computed by Moosavi et al. (2020) [Moosavi2020]_.
-    The raw labels and structures can accessed also on
-    `MaterialsCloud <https://archive.materialscloud.org/record/2020.67>`_.
+    Reproduced from [Nandy2022]_.
+    Nandy et al. (2022) digitized traces from thermogravimetric analysis.
+    The decomposition temperature they determined in this way is reported in
+    `outputs.assigned_T_decomp`.
 
     To reduce the risk of data leakage, we  (by default) also only keep one representative
     structure for a "base refcode" (i.e. the first five letters of a refcode).
@@ -34,47 +35,22 @@ class CoREDataset(AbstractStructureDataset):
     for the derivation of the structure graph (e.g. the structure
     graph of ULOMAL occurs 59 in the CoRE database used by Moosavi et al.).
 
-    .. warning::
-        Even though we performed some basic sanity checks, there are currently still
-        some structures that might chemically not be reasonable.
-        Also, even though we only keep one structure per base refcode, there is still
-        potential for data leakge. We urge users to still drop duplicates (or close neighbors)
-        after featurization.
-
-        If this set is used as test set, make sure to drop all overlapping entries in your training set.
-
     The years refer to the publication dates of the paper crossreferenced
-    in the CSD entry of the structure. We excluded structures that are not
-    deposited in the CSD.
+    in the CSD entry of the structure.
 
     The available labels are:
 
-        * 'pure_CO2_kH': Henry coefficient of CO2 obtained by Widom method in mol kg-1 Pa-1
-        * 'pure_CO2_widomHOA': Heat of adsorption of CO2 obtained by Widom method in
-        * 'pure_methane_kH': Henry coefficient of methane obtained by Widom method in mol kg-1 Pa-1
-        * 'pure_methane_widomHOA': Heat of adsorption of methane obtained by Widom method
-        * 'pure_uptake_CO2_298.00_15000': Pure CO2 uptake at 298.00 K and 15000 Pa in mol kg-1
-        * 'pure_uptake_CO2_298.00_1600000': Pure CO2 uptake at 298.00 K and 1600000 Pa in mol kg-1
-        * 'pure_uptake_methane_298.00_580000': Pure methane uptake at 298.00 K and 580000 Pa in mol kg-1
-        * 'pure_uptake_methane_298.00_6500000': Pure methane uptake at 298.00 K and 6500000 Pa in mol kg-1
-        * 'logKH_CO2': Logarithm of Henry coefficient of CO2 obtained by Widom method
-        * 'logKH_CH4': Logarithm of Henry coefficient of methane obtained by Widom method
-        * 'CH4DC': CH4 deliverable capacity in vSTP/v
-        * 'CH4HPSTP': CH4 high pressure uptake in standard temperature and pressure in vSTP/v
-        * 'CH4LPSTP': CH4 low pressure uptake in standard temperature and pressure in vSTP/v
+        * `outputs.assigned_T_decomp`: Decomposition temperature in Kelvin.
 
-    References:
-        .. [Moosavi2020] Moosavi, S. M.; Nandy, A.; Jablonka, K. M.; Ongari, D.; Janet, J. P.; Boyd, P. G.; Lee,
-            Y.; Smit, B.; Kulik, H. J. Understanding the Diversity of the Metal-Organic Framework Ecosystem.
-            Nature Communications 2020, 11 (1), 4068. https://doi.org/10.1038/s41467-020-17755-8.
-
+    References::
+        .. [Nandy2022] `Nandy, A.; Terrones, G.; Arunachalam, N.; Duan, C.; Kastner, D. W.; Kulik, H. J. MOFSimplify, Machine Learning Models with Extracted Stability Data of Three Thousand Metal–Organic Frameworks. Scientific Data 2022, 9 (1). <https://doi.org/10.1038/s41597-022-01181-0>`_
     """
 
     _files = {
         "v0.0.1": {
-            "df": "https://zenodo.org/record/7032358/files/data.json?download=1",
-            "structures": "https://zenodo.org/record/7032358/files/structures.tar.gz?download=1",
-            "expected_length": 5393,
+            "df": "https://www.dropbox.com/s/68z3d7wxh3lojrr/data.json?dl=1",
+            "structures": "https://www.dropbox.com/s/yd2s7fd8qp257w3/structures.tar.gz?dl=1",
+            "expected_length": 2039,
         }
     }
 
@@ -84,25 +60,8 @@ class CoREDataset(AbstractStructureDataset):
         drop_basename_duplicates: bool = True,
         drop_graph_duplicates: bool = True,
         subset: Optional[Collection[int]] = None,
-        drop_nan: bool = True,
+        drop_nan: bool = False,
     ):
-        """Construct an instance of the CoRE dataset.
-
-        Args:
-            version (str): version number to use.
-                Defaults to "v0.0.1".
-            drop_basename_duplicates (bool): If True, keep only one structure
-                per CSD basename. Defaults to True.
-            drop_graph_duplicates (bool): If True, keep only one structure
-                per decorated graph hash. Defaults to True.
-            subset (Collection[int], optional): indices of the structures to include.
-                Defaults to None.
-            drop_nan (bool): If True, drop rows with NaN values in features or hashes.
-                Defaults to True.
-
-        Raises:
-            ValueError: If the provided version number is not available.
-        """
         self._drop_basename_duplicates = drop_basename_duplicates
         self._drop_nan = drop_nan
         self._drop_graph_duplicates = drop_graph_duplicates
@@ -113,7 +72,7 @@ class CoREDataset(AbstractStructureDataset):
         self.version = version
 
         self._structure_dir = MOFDSCRIBE_PYSTOW_MODULE.ensure_untar(
-            "CoRE",
+            "thermal-stability",
             self.version,
             name="structures.tar.gz",
             url=self._files[version]["structures"],
@@ -121,7 +80,7 @@ class CoREDataset(AbstractStructureDataset):
 
         self._df = pd.DataFrame(
             MOFDSCRIBE_PYSTOW_MODULE.ensure_json(
-                "CoRE", self.version, name="data.json", url=self._files[version]["df"]
+                "thermal-stability", self.version, name="data.json", url=self._files[version]["df"]
             )
         ).reset_index(drop=True)
 
@@ -155,7 +114,7 @@ class CoREDataset(AbstractStructureDataset):
             self._df = self._df.reset_index(drop=True)
 
         self._structures = [
-            os.path.join(self._structure_dir, f + ".cif") for f in self._df["info.name"]
+            os.path.join(self._structure_dir, f + ".cif") for f in self._df["info.CoRE_name"]
         ]
 
         check_all_file_exists(self._structures)
@@ -170,6 +129,7 @@ class CoREDataset(AbstractStructureDataset):
         self._featurenames = (c for c in self._df.columns if c.startswith("features."))
         self._infonames = (c for c in self._df.columns if c.startswith("info."))
 
+
     def get_subset(self, indices: Collection[int]) -> "AbstractStructureDataset":
         """Get a subset of the dataset.
 
@@ -180,13 +140,14 @@ class CoREDataset(AbstractStructureDataset):
             AbstractStructureDataset: a new dataset containing only the structures
                 specified by the indices.
         """
-        return CoREDataset(
+        return ThermalStabilityDataset(
             version=self.version,
             drop_basename_duplicates=self._drop_basename_duplicates,
             drop_graph_duplicates=self._drop_graph_duplicates,
             subset=indices,
             drop_nan=self._drop_nan,
         )
+
 
     @property
     def available_info(self) -> Tuple[str]:
@@ -207,20 +168,6 @@ class CoREDataset(AbstractStructureDataset):
     @property
     def citations(self) -> Tuple[str]:
         return [
-            "@article{Moosavi2020,"
-            "doi = {10.1038/s41467-020-17755-8},"
-            "url = {https://doi.org/10.1038/s41467-020-17755-8},"
-            "year = {2020},"
-            "month = aug,"
-            "publisher = {Springer Science and Business Media {LLC}},"
-            "volume = {11},"
-            "number = {1},"
-            "author = {Seyed Mohamad Moosavi and Aditya Nandy and Kevin Maik Jablonka "
-            "and Daniele Ongari and Jon Paul Janet and Peter G. Boyd and Yongjin Lee "
-            "and Berend Smit and Heather J. Kulik},"
-            "title = {Understanding the diversity of the metal-organic framework ecosystem},"
-            "journal = {Nature Communications}"
-            "}",
             "@article{Chung2019,"
             "doi = {10.1021/acs.jced.9b00835},"
             "url = {https://doi.org/10.1021/acs.jced.9b00835},"
@@ -255,5 +202,17 @@ class CoREDataset(AbstractStructureDataset):
             "title = {Computation-Ready,  Experimental Metal{\textendash}Organic Frameworks: "
             "A Tool To Enable High-Throughput Screening of Nanoporous Crystals},"
             "journal = {Chemistry of Materials}"
+            "}",
+            "@article{Nandy_2022,"
+            "doi = {10.1038/s41597-022-01181-0},"
+            "url = {https://doi.org/10.1038%2Fs41597-022-01181-0},"
+            "year = 2022,"
+            "month = {mar},"
+            "publisher = {Springer Science and Business Media {LLC}},"
+            "volume = {9},"
+            "number = {1},"
+            "author = {Aditya Nandy and Gianmarco Terrones and Naveen Arunachalam and Chenru Duan and David W. Kastner and Heather J. Kulik},"
+            "title = {{MOFSimplify}, machine learning models with extracted stability data of three thousand metal{\textendash}organic frameworks},"
+            "journal = {Sci Data}"
             "}",
         ]
